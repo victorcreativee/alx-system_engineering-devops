a321c313814cf27855a854be1e59280013bd5735
victorcreativee <tuyizerevictor@gmail.com>
2025-01-11 01:28:46 +0200

    create a puppet manifest

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#This is puppet manifest that creates a file
+
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
