08bd22a75d3fe1e44f44179ea770e646a92611c2
victorcreativee <tuyizerevictor@gmail.com>
2025-02-11 00:27:56 +0200

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,34 +9,35 @@ Script that exports an employee's TODO list data to a CSV file.
 import requests
 import sys
 import csv
+ef get_employee_todos_progress(employee_id):
+    """ a function to return the info about the employee todos progress"""
+    try:
+        url = "https://jsonplaceholder.typicode.com/"
+        user_datas = requests.get(url + f"users/{employee_id}")
+        user_data = user_datas.json()
+        employee_name = user_data['name']
+
+        """fetch todos list for the employee"""
+        todos_list = requests.get(url + f"todos?userId={employee_id}")
+        json_todos_list = todos_list.json()
+
+        total_task = len(json_todos_list)
+        task_done = [task for task in json_todos_list if task['completed']]
+        no_task_done = len(task_done)
+
+        """display the result"""
+        print(f"Employee {employee_name} is done with tasks("
+              f"{no_task_done}/{total_task}):")
+
+        """title of completed task"""
+        for task in task_done:
+            print(f"\t {task['title']}")
+    except Exception as e:
+        print(f"an error ocuured: {e}")
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 1-export_to_CSV.py <employee_id>")
-        sys.exit(1)
-
-    user_id = sys.argv[1]
-
-    base_url = "https://jsonplaceholder.typicode.com"
-
-
-    user_res = requests.get(f"{base_url}/users/{user_id}")
-    if user_res.status_code != 200:
-        print("Error: User not found")
-        sys.exit(1)
-
-    user_data = user_res.json()
-    username = user_data.get("username")
-
-
-    todos_res = requests.get(f"{base_url}/todos?userId={user_id}")
-    todos = todos_res.json()
-
-
-    filename = f"{user_id}.csv"
-    with open(filename, mode="w", newline="") as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        for task in todos:
-            writer.writerow([user_id, username, task.get("completed"), task.get("title")])
-
-    print(f"Data exported to {filename}")
+    if len(argv) != 2:
+        print("Usage: Script <employee_id>")
+    else:
+        get_employee_todos_progress(argv[1])
